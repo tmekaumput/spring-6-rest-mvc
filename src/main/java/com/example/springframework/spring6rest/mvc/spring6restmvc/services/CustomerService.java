be533a84c58c935b9a8133a7774381864034404e
Kampanart Mekaumput <kampanartm99@gmail.com>
2024-04-03 22:18:54 +1100

    Create Service, use Map to store 2-3 customers

---
+++ src/main/java/com/example/springframework/spring6rest/mvc/spring6restmvc/services/CustomerService.java
@@ -0,0 +1,18 @@
+package com.example.springframework.spring6rest.mvc.spring6restmvc.services;
+/*
+ * @Author tmekaumput
+ * @Date 3/4/24 9:59 pm
+ *
+ */
+
+import com.example.springframework.spring6rest.mvc.spring6restmvc.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> listCustomers();
+
+
+
+    Customer getCustomerById(Integer id);
+}
---
+++ src/main/java/com/example/springframework/spring6rest/mvc/spring6restmvc/services/CustomerServiceImpl.java
@@ -0,0 +1,55 @@
+package com.example.springframework.spring6rest.mvc.spring6restmvc.services;
+/*
+ * @Author tmekaumput
+ * @Date 3/4/24 10:00 pm
+ *
+ */
+
+import com.example.springframework.spring6rest.mvc.spring6restmvc.model.Customer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.*;
+
+@Slf4j
+@Service
+public class CustomerServiceImpl implements CustomerService {
+
+    private final Map<Integer, Customer> customerMap;
+
+    public CustomerServiceImpl() {
+        customerMap = new HashMap<>();
+
+        Customer c1 = Customer.builder().id(1)
+        .customerName("B1")
+        .version(1)
+        .createdDate(new Date())
+        .lastModifiedDate(new Date()).build();
+
+        Customer c2 = Customer.builder().id(2)
+                .customerName("B2")
+                .version(1)
+                .createdDate(new Date())
+                .lastModifiedDate(new Date()).build();
+
+        Customer c3 = Customer.builder().id(3)
+                .customerName("B3")
+                .version(1)
+                .createdDate(new Date())
+                .lastModifiedDate(new Date()).build();
+
+        customerMap.put(c1.getId(), c1);
+        customerMap.put(c2.getId(), c2);
+        customerMap.put(c3.getId(), c3);
+    }
+
+    @Override
+    public List<Customer> listCustomers() {
+        return new ArrayList<>(customerMap.values());
+    }
+
+    @Override
+    public Customer getCustomerById(Integer id) {
+        return customerMap.get(id);
+    }
+}
