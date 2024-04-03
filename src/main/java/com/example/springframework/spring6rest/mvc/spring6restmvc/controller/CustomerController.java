4a239e3d0c2b4278ab097c9ef8e42596de776767
Kampanart Mekaumput <kampanartm99@gmail.com>
2024-04-04 10:38:27 +1100

    Create Controller Endpoint to get customer by id

--- src/main/java/com/example/springframework/spring6rest/mvc/spring6restmvc/controller/CustomerController.java
+++ src/main/java/com/example/springframework/spring6rest/mvc/spring6restmvc/controller/CustomerController.java
@@ -9,6 +9,7 @@ import com.example.springframework.spring6rest.mvc.spring6restmvc.model.Customer
 import com.example.springframework.spring6rest.mvc.spring6restmvc.services.CustomerService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -25,5 +26,12 @@ public class CustomerController {
     @RequestMapping(method = RequestMethod.GET)
     public List<Customer> getCustomers() {
         return customerService.listCustomers();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "{id}")
+    public Customer getCustomerById(@PathVariable("id") Integer id) {
+        log.debug("Retrieving customer : " + id);
+
+        return customerService.getCustomerById(id);
     }
 }
