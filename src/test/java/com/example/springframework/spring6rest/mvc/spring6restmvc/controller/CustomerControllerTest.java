c59e4d6c764f326a6f4ae29c5ec1adc13196bd11
Kampanart Mekaumput <kampanartm99@gmail.com>
2024-04-05 17:13:23 +1100

    Use Spring MockMVC and Mockito to test GET operations for customer

---
+++ src/test/java/com/example/springframework/spring6rest/mvc/spring6restmvc/controller/CustomerControllerTest.java
@@ -0,0 +1,9 @@
+package com.example.springframework.spring6rest.mvc.spring6restmvc.controller;
+/*
+ * @Author tmekaumput
+ * @Date 5/4/24 5:13 pm
+ *
+ */
+
+public class CustomerControllerTest {
+}
