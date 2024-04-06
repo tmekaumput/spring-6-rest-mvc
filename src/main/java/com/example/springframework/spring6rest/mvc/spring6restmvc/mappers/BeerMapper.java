9d4e954698675109dfe13e43949bbb8939a46d9f
Kampanart Mekaumput <kampanartm99@gmail.com>
2024-04-06 20:00:23 +1100

    Add beer and customer dto mappers

---
+++ src/main/java/com/example/springframework/spring6rest/mvc/spring6restmvc/mappers/BeerMapper.java
@@ -0,0 +1,18 @@
+package com.example.springframework.spring6rest.mvc.spring6restmvc.mappers;
+/*
+ * @Author tmekaumput
+ * @Date 6/4/24 7:57 pm
+ *
+ */
+
+import com.example.springframework.spring6rest.mvc.spring6restmvc.entities.Beer;
+import com.example.springframework.spring6rest.mvc.spring6restmvc.model.BeerDTO;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface BeerMapper {
+
+    Beer beerDtoToBeer(BeerDTO dto);
+
+    BeerDTO beerToBeerDto(Beer beer);
+}
