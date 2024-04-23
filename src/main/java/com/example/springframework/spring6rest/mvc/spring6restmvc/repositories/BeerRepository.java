e963664e0fc1edbc0517fe3860c7b04e5af41f63
Kampanart Mekaumput <kampanartm99@gmail.com>
2024-04-23 10:20:54 +1000

    Annotate Beer repository with path and set the collection in the response

--- src/main/java/com/example/springframework/spring6rest/mvc/spring6restmvc/repositories/BeerRepository.java
+++ src/main/java/com/example/springframework/spring6rest/mvc/spring6restmvc/repositories/BeerRepository.java
@@ -10,9 +10,11 @@ import com.example.springframework.spring6rest.mvc.spring6restmvc.model.BeerStyl
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
+@RepositoryRestResource(path = "beer", collectionResourceRel = "beers")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
 
     Page<Beer> findAllByBeerNameIsLikeIgnoreCase(String beerName, Pageable pageable);
