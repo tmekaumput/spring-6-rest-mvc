564ea703f3a04f1680271601cdd9aac03501da56
Kampanart Mekaumput <kampanartm99@gmail.com>
2024-04-25 15:34:38 +1000

    Add and update Beer Client test cases for more coverage

--- src/test/java/com/example/springframework/spring6rest/resttemplate/client/BeerClientImplTest.java
+++ src/test/java/com/example/springframework/spring6rest/resttemplate/client/BeerClientImplTest.java
@@ -1,8 +1,13 @@
 package com.example.springframework.spring6rest.resttemplate.client;
 
+import com.example.springframework.spring6rest.mvc.spring6restmvc.model.BeerDTO;
+import com.example.springframework.spring6rest.mvc.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 /*
  * @Author tmekaumput
@@ -13,22 +18,54 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class BeerClientImplTest {
 
+    private static final Integer PAGE_SIZE = 20;
+
     @Autowired
     BeerClientImpl beerClient;
 
     @Test
     void listBeers() {
-        beerClient.listBeers();
+
+        Page<BeerDTO> beers = beerClient.listBeers();
+
+        assertThat(beers.getSize()).isGreaterThan(0);
     }
 
     @Test
     void listBeersNoBeerName() {
 
-        beerClient.listBeers(null, null, null, null, null);
+        Page<BeerDTO> beers = beerClient.listBeers(null, null, null, null, null);
+
+        assertThat(beers.getSize()).isGreaterThan(0);
     }
 
     @Test
     void listBeersByName() {
-        beerClient.listBeers("ALE", null, null, null, null);
+
+        Page<BeerDTO> beers = beerClient.listBeers("ALE", null, null, null, null);
+
+        assertThat(beers.getSize()).isGreaterThan(0);
     }
+
+    @Test
+    void listBeersByStyle() {
+        Page<BeerDTO> beers = beerClient.listBeers(null, BeerStyle.LAGER, null, null, null);
+
+        assertThat(beers.getSize()).isGreaterThan(0);
+    }
+
+    @Test
+    void listBeersByNameAndStyle() {
+        Page<BeerDTO> beers = beerClient.listBeers("ALE", BeerStyle.LAGER, null, null, null);
+
+        assertThat(beers.getSize()).isGreaterThan(0);
+    }
+
+    @Test
+    void listBeersWithPageSize() {
+        Page<BeerDTO> beers = beerClient.listBeers(null, null, null, 0, PAGE_SIZE);
+
+        assertThat(beers.getSize()).isEqualTo(PAGE_SIZE);
+    }
+
 }
